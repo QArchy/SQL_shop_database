ae35c5acc0b1fb76be79ffa0368018ccd8c19aaa
Artyom <namoma@mail.ru>
2022-04-04 17:35:27 +0000

    Initial

---
+++ Shop.sql
@@ -0,0 +1,14 @@
+CREATE SCHEMA 'shop' DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+CREATE TABLE `shop`.`category` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(128) NOT NULL,
+  `discount` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`));
+  
+ALTER TABLE `shop`.`category` 
+ADD COLUMN `alias_name` VARCHAR(128) NULL AFTER `discount`;
+
+DROP TABLE `shop`.`category`;
+
+DROP DATABASE `shop`;
